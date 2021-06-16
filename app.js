43dc94f9ca9458853f7fada4bb422b9d8c6ca951
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-06-17 00:29:42 +0500

    JS File

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var xyz = document.getElementById('fn')
-function submitData(num) {
-    xyz.value += num
-}
+// function submitData(num) {
+//     xyz.value += num
+// }
 function clearResult() {
     xyz.value = ""
 }
@@ -11,11 +11,15 @@ function getResult() {
     }
 
 function clearLast(){
- xyz.value = xyz.value.slice(0, - 1);
+ xyz.value = xyz.value.slice(0,- 1);
 }
-
-
-function setvalue(num){
-var val = document.getElemntById("val")
-
+function submitData(num){
+var lastVal = xyz.value.slice(xyz.value.length - 1);
+var operators = ["+","-","/","*"];
+if (operators.indexOf(lastVal) !== -1 && operators.indexOf(num) !== -1){
+    xyz.value = xyz.value.slice(0, xyz.value - 1) 
+}
+else{
+    xyz.value +=num
+}
 }
