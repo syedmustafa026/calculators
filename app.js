bfe74cde3298359cbd910717788be3276ffaf307
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-06-17 10:21:26 +0500

    JS File

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function submitData(num){
 var lastVal = xyz.value.slice(xyz.value.length - 1);
 var operators = ["+","-","/","*"];
 if (operators.indexOf(lastVal) !== -1 && operators.indexOf(num) !== -1){
-    xyz.value = xyz.value.slice(0, xyz.value - 1) 
+    xyz.value = xyz.value.slice(0, xyz.value.length - 1) + num
 }
 else{
     xyz.value +=num
